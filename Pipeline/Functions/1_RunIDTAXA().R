bfcb6356de26d23133344c836619a45e8954b9f7
J <j.williams20@imperial.ac.uk>
2021-10-28 19:05:08 +0100

    Improved RunIDTAXA wrt ranks
    modification so that there are two options for RunIDTAXA dependig on whether the training set includes rank info. This improves behaviour as the rank info is used if available ( if a taxid was used to create the trainingSet). If the rank info is not available then ranks are assumed.

--- Pipeline/Functions/1_RunIDTAXA().R
+++ Pipeline/Functions/1_RunIDTAXA().R
@@ -15,15 +15,40 @@ RunIdtaxa<-function(Type, trainingSet, TableToMergeTo=LuluOutput, SeqsToAssign=S
 
             ids <- IdTaxa(dna,
                         trainingSet,
+                        type="extended",
                         strand="top",
                         threshold=threshold,
                         processors=4)
 
-            #convert output to neat dataframe
+
+            #convert output to neat dataframe dependent on whether trainingset includes rank information (derived from a taxid file)
+            # or not, if it doesn't then currently rank assignments are assumed (not ideal).
+            
+            desiredRanks<-c("domain", "kingdom", "phylum", "class", "order", "family", "genus")
+
+            if (is.null(trainingSet$rank)) {
+
                 IDtaxa_df<-bind_rows(lapply(ids,unlist), .id = "ESV")
-                names(IDtaxa_df)[2:8]<-c("rootrank", "domain", "phylum", "class", "order", "family", "genus")
-                names(IDtaxa_df)[9:15]<-paste0("confidence ", c("rootrank", "domain", "phylum", "class", "order", "family", "genus"))
-                IDtaxa_df<-IDtaxa_df[-c(16:22)]
+                IDtaxa_df<-cbind(IDtaxa_df$ESV, select(IDtaxa_df, starts_with( "taxon")), select(IDtaxa_df, starts_with( "confidence")))
+                names(IDtaxa_df)[1]<-"ESV"
+                names(IDtaxa_df)[2:  (length(desiredRanks)+1)]<-desiredRanks
+                names(IDtaxa_df)[ (length(desiredRanks)+2) : (length(desiredRanks)*2+1) ]<-paste0("confidence ", desiredRanks)
+                IDtaxa_df<-IDtaxa_df[ 1: (length(desiredRanks)*2+1) ]
+
+            } else {
+                
+                IDtaxa_df<-data.frame(matrix(NA, ncol=2*length(desiredRanks), nrow=0))
+                names(IDtaxa_df)<-c(desiredRanks, paste0(desiredRanks, "_confidence"))
+                
+                for (i in 1: length(ids)) {
+                    selection<-ids[[i]]$rank %in% desiredRanks
+                    insertions<-desiredRanks %in% ids[[i]]$rank
+                    IDtaxa_df[i,1:length(desiredRanks)][insertions]<-ids[[i]]$taxon[selection]
+                    IDtaxa_df[i, (length(desiredRanks)+1) : (length(desiredRanks)*2) ] [insertions] <-ids[[i]]$confidence[selection]
+                }
+                IDtaxa_df$ESV<-rownames(IDtaxa_df)
+
+            }
 
             #merge with seq data table
                 TableToMergeTo$ESV<-rownames(TableToMergeTo)
