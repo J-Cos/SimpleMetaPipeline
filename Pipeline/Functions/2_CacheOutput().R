15243d03bfcd1883acd99b278ec020b7a7931730
J <j.williams20@imperial.ac.uk>
2021-11-10 17:54:21 +0000

    CacheOutput error fixed
    no longer fails if object to be cached is assigned NULL

--- Pipeline/Functions/2_CacheOutput().R
+++ Pipeline/Functions/2_CacheOutput().R
@@ -1,3 +1,5 @@
 CacheOutput<-function(object){
-    saveRDS(object, file=file.path(path, "IntermediateOutputs", paste0(dataname,"_", deparse(substitute(object)),".RDS")))
+    if (!is.null(object)) {
+        saveRDS(object, file=file.path(path, "IntermediateOutputs", paste0(dataname,"_", deparse(substitute(object)),".RDS")))
+    }
 }
