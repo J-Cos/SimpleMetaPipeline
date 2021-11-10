392c4acaaa80f7c94d9d2711b80f37b7beb11879
J <j.williams20@imperial.ac.uk>
2021-11-10 17:59:56 +0000

    ensure RunLulu outputs SeqDataTable
    make sure ESV data is included in LULU Output, it was previously only added at the idtaxa step.

--- Pipeline/Functions/1_RunLULU().R
+++ Pipeline/Functions/1_RunLULU().R
@@ -14,6 +14,11 @@ RunLULU<-function(TableToMergeTo) {
             SeqDataTable<-merge(TableToMergeTo, curated_OTU_map, by= "OTU", all=TRUE)
             SeqDataTable$CuratedOTURepresentativeSequence<-SeqDataTable$CuratedOTURepresentativeSequence==TRUE & SeqDataTable$OTUrepresentativeSequence==TRUE
 
+
+           #merge with seq data table
+                SeqDataTable$ESV<-rownames(SeqDataTable)
+                SeqDataTable$ESV<-paste0("ESV_", SeqDataTable$ESV)
+
         return(SeqDataTable)
 
 
