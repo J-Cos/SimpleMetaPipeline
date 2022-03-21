1fa3f5825794906c95e60f1103699387d88e4df7
J <j.williams20@imperial.ac.uk>
2022-03-21 12:10:16 +0000

    CacheOutput changed to cache null outputs
    important as when restarting pipeline downstream there needs to be a record of each step, even if it was not used.

--- Pipeline/Functions/2_CacheOutput().R
+++ Pipeline/Functions/2_CacheOutput().R
@@ -1,5 +1,5 @@
 CacheOutput<-function(object){
-    if (!is.null(object)) {
+    #saves even if null to ensure that result printing is able to confirm which steps were not chosen to be completed
         saveRDS(object, file=file.path(path, "IntermediateOutputs", paste0(dataname,"_", deparse(substitute(object)),".RDS")))
-    }
+
 }
