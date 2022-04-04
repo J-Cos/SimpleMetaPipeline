7eb7e685e9c7c6331d73af59d790ef33867d9d38
J <j.williams20@imperial.ac.uk>
2022-04-04 14:30:54 +0100

    Update SeqDataTable2Phyloseq().R
    Improved code and multi-assignment functionality added.

--- SupportFunctions/SeqDataTable2Phyloseq().R
+++ SupportFunctions/SeqDataTable2Phyloseq().R
@@ -1,154 +1,131 @@
 #dependencies 
     #phyloseq
+    #tidyverse
+    #DECIPHER
 
 # function to take output of bioinformatic pipeline into phyloseq
     #example
-        #example<-SeqDataTable2Phyloseq(SeqDataTablefile="23s_test_SeqDataTable.RDS", clustering="cOTU", Metadata=NULL)
+        #example<-SeqDataTable2Phyloseq(SeqDataTablefile="23s_test_SeqDataTable.RDS", clustering="curatedOTU", Metadata=NULL, assignment="BLAST")
 
-SeqDataTable2Phyloseq<-function(SeqDataTablePath, clustering, Metadata=NULL){
+#clustering can be ESV, curatedESV, OTU, curatedOTU
+#assignment can be BLAST, Idtaxa or None
 
-    SeqDataTable<-readRDS(SeqDataTablePath)
+SeqDataTable2Phyloseq<-function(SeqDataTablePath, clustering, Metadata=NULL, assignment="None"){
 
-    # very explicit column math to ensure 
-        #1) that this function works with variable numbers of samples
-        #2) that it is clear how 
-        numberstandardcolumns<- 6
-        numSamples<-which(names(SeqDataTable)=="CuratedOTURepresentativeSequence") -numberstandardcolumns
+    #check clustering valid, break if not
+        validclusterings<-c("ESV", "curatedESV", "OTU", "curatedOTU")
+        if (!clustering  %in%  validclusterings) { print(paste0(clustering, " clustering choice invalid please choose one of: ", validclusterings)) ; return(NULL)}
+        validassignments<-c("BLAST", "Idtaxa", "None")
+        if (!assignment  %in%  validassignments) { print(paste0( assignment, " assignment choice invalid please choose one of: ", validassignments)) ; return(NULL) }
 
-        firstTaxCol<-numberstandardcolumns + numSamples + 1
-        lastConfCol<-length(names(SeqDataTable))
-        numRanks<-(lastConfCol-firstTaxCol+1)/2
-        lastTaxCol<-firstTaxCol+numRanks-1
-        firstConfCol<-lastConfCol-numRanks
+    #preparatory steps
 
-    #main function if statements creating ps components
-        if (clustering == "ESV") {
+        SeqDataTable<-readRDS(SeqDataTablePath)
 
-            #otumat
-                otumat<-SeqDataTable %>% 
-                    group_by( ESV ) %>% 
-                    summarise_if(is.numeric,sum) %>% 
-                    column_to_rownames("ESV")%>% 
-                    as.matrix() 
-
-                reformatSampleNames<-function(list_item) {
-                    string<-unlist(strsplit(list_item, "_"))
-                    newstring<-string[c(-1, (-length(string)+1):-length(string))]
-                    newstring<-paste(newstring, collapse="_")
-                    return(newstring)
-                }
-                colnames(otumat)<-lapply(colnames(otumat), reformatSampleNames)
+        #rename this column to match all others - should be fixed downstream later
+        names(SeqDataTable)[names(SeqDataTable)=="OTUrepresentativeSequence"]<-"OTURepresentativeSequence"
 
 
-            if(firstTaxCol!=lastConfCol) {
-                #taxmat
-                taxmat<-SeqDataTable[,firstTaxCol:lastTaxCol]
+        #get seq datatable and reorder columns
+            clusteringcolumns<- c(  "ESV",
+                                    "Sequence",
+                                    "curatedESV",                      
+                                    "CuratedESVRepresentativeSequence",
+                                    "OTU",
+                                    "OTURepresentativeSequence",       
+                                    "curatedOTU",                       
+                                    "CuratedOTURepresentativeSequence")
+            
+            #ensure standard order to clustering cols as above
+            SeqDataTable<-SeqDataTable %>% relocate(clusteringcolumns)
+
+        #recode clustering variables to work with tidyverse as arguments
+            symclustering<-sym(clustering)
+            RepresentativeSequenceColumnName<-paste0(toupper(substr(clustering, 1, 1)), substr(clustering, 2, nchar(clustering)), sep="", "RepresentativeSequence")
+
+        ## get indices
+            MainIndices<-which(names(SeqDataTable) %in% clusteringcolumns)
+            SampleIndices<-grep("Sample_", colnames(SeqDataTable))
+            if (assignment =="BLAST") {
+                AssignmentIndices<-( grep("Blast_query_coverage", colnames(SeqDataTable)) +1 ) : dim(SeqDataTable)[2]
+            } else if (assignment== "Idtaxa") {
+                AssignmentIndices <- ( last(SampleIndices) + 1 ) : ( grep("_Confidence", colnames(SeqDataTable))[1] -1 )
+            }
+
+
+    #create input matrices
+        #otumat
+        otumat<-SeqDataTable %>% 
+            group_by( !!symclustering ) %>% 
+            summarise_if(is.numeric,sum) %>% 
+            column_to_rownames(clustering)%>% 
+            as.matrix()
+
+
+        if (clustering=="ESV") {
+              taxmat<-SeqDataTable[,AssignmentIndices]
                 taxmat$cluster<-SeqDataTable$ESV
                 taxmat<-arrange(taxmat, cluster) %>%
                     column_to_rownames("cluster") %>%
                     as.matrix()
-            }
-
-            #refseqs
-            refseqs_df<-SeqDataTable[,c("Sequence","ESV")] %>%
-                arrange(, ESV) %>%
-                column_to_rownames("ESV")
-            refseqs <- DNAStringSet(refseqs_df$Sequence)
-            names(refseqs)<-rownames(refseqs_df)
-
-
-        } else if (clustering == "OTU") {
-
-            #otumat
-            otumat<-SeqDataTable %>% 
-                group_by( OTU ) %>% 
-                summarise_if(is.numeric,sum) %>% 
-                column_to_rownames("OTU")%>% 
-                as.matrix() 
-
-            if(firstTaxCol!=lastConfCol) {
-                #taxmat
-                taxmat<-SeqDataTable[SeqDataTable$OTUrepresentativeSequence==TRUE,][,firstTaxCol:lastTaxCol]
-                taxmat$cluster<-SeqDataTable$OTU[SeqDataTable$OTUrepresentativeSequence==TRUE]
-                taxmat<-arrange(taxmat, cluster) %>%
-                    column_to_rownames("cluster") %>%
-                    as.matrix()
-            }
-
-            #refseqs
-            refseqs_df<-SeqDataTable[SeqDataTable$OTUrepresentativeSequence==TRUE,c("sequence","OTU")] %>%
-                arrange(, OTU) %>%
-                column_to_rownames("OTU")
-            refseqs <- DNAStringSet(refseqs_df$sequence)
-            names(refseqs)<-rownames(refseqs_df)
-
-        } else if (clustering =="cOTU") {
-
-            #otumat
-            otumat<-SeqDataTable %>% 
-                group_by( curatedOTU ) %>% 
-                summarise_if(is.numeric,sum) %>% 
-                column_to_rownames("curatedOTU")%>% 
+        } else {
+            taxmat<-SeqDataTable[SeqDataTable[[RepresentativeSequenceColumnName]]==TRUE,][,AssignmentIndices]
+            taxmat$cluster<-SeqDataTable[[clustering]][SeqDataTable[[RepresentativeSequenceColumnName]]==TRUE]
+            taxmat<-arrange(taxmat, cluster) %>%
+                column_to_rownames("cluster") %>%
                 as.matrix()
-            
-            if(firstTaxCol!=lastConfCol) {
-                #taxmat
-                taxmat<-SeqDataTable[SeqDataTable$CuratedOTURepresentativeSequence==TRUE,][,firstTaxCol:lastTaxCol]
-                taxmat$cluster<-SeqDataTable$curatedOTU[SeqDataTable$CuratedOTURepresentativeSequence==TRUE]
-                taxmat<-arrange(taxmat, cluster) %>%
-                    column_to_rownames("cluster") %>%
-                    as.matrix()
-            }
-
-            #refseqs
-            refseqs_df<-SeqDataTable[SeqDataTable$CuratedOTURepresentativeSequence==TRUE,c("sequence","curatedOTU")] %>%
-                arrange(, curatedOTU) %>%
-                column_to_rownames("curatedOTU")
-            refseqs <- DNAStringSet(refseqs_df$sequence)
-            names(refseqs)<-rownames(refseqs_df)
-
-        } else { 
-            print("unrecognised clustering") 
         }
 
-    #build ps if statements
-        if (!exists("taxmat")) {
-            if (is.null(Metadata)){
-                ps<-phyloseq(
-                    # sample_data(),
-                    otu_table(otumat, taxa_are_rows=TRUE),
-                    #tax_table(taxmat),
-                    #phy_tree(),
-                    refseq(refseqs)
-                )
-            } else {
-                ps<-phyloseq(
-                    sample_data(Metadata),
-                    otu_table(otumat, taxa_are_rows=TRUE),
-                    #tax_table(taxmat),
-                    #phy_tree(),
-                    refseq(refseqs)
-                ) 
-            }
-        } else {
-            if (is.null(Metadata)){
-                ps<-phyloseq(
-                    # sample_data(),
-                    otu_table(otumat, taxa_are_rows=TRUE),
-                    tax_table(taxmat),
-                    #phy_tree(),
-                    refseq(refseqs)
-                )
-            } else {
-                ps<-phyloseq(
-                    sample_data(Metadata),
-                    otu_table(otumat, taxa_are_rows=TRUE),
-                    tax_table(taxmat),
-                    #phy_tree(),
-                    refseq(refseqs)
-                ) 
-            }
 
+        #refseqs
+        if (clustering=="ESV") {
+              refseqs_df<-SeqDataTable[,c("Sequence",clustering)] %>%
+            arrange(, !!symclustering ) %>%
+            column_to_rownames(clustering)
+            refseqs <- DNAStringSet(refseqs_df$Sequence)
+            names(refseqs)<-rownames(refseqs_df)
+        } else {
+            refseqs_df<-SeqDataTable[SeqDataTable[[RepresentativeSequenceColumnName]]==TRUE,c("Sequence",clustering)] %>%
+            arrange(, !!symclustering ) %>%
+            column_to_rownames(clustering)
+            refseqs <- DNAStringSet(refseqs_df$Sequence)
+            names(refseqs)<-rownames(refseqs_df)
+        }
+
+        
+    #create phyloseq object depending on which matrices present
+        if (assignment=="None" & is.null(Metadata)) {
+            ps<-phyloseq(
+                        #sample_data(Metadata),
+                        otu_table(otumat, taxa_are_rows=TRUE),
+                        #tax_table(taxmat),
+                        #phy_tree(),
+                        refseq(refseqs) 
+                    )
+        } else if (assignment!="None" & is.null(Metadata)) {
+            ps<-phyloseq(
+                        #sample_data(Metadata),
+                        otu_table(otumat, taxa_are_rows=TRUE),
+                        tax_table(taxmat),
+                        #phy_tree(),
+                        refseq(refseqs) 
+                    )
+        } else if (assignment=="None" & !is.null(Metadata)) {
+            ps<-phyloseq(
+                        sample_data(Metadata),
+                        otu_table(otumat, taxa_are_rows=TRUE),
+                        #tax_table(taxmat),
+                        #phy_tree(),
+                        refseq(refseqs) 
+                    )
+        } else if (assignment!="None" & !is.null(Metadata)) {
+            ps<-phyloseq(
+                        sample_data(Metadata),
+                        otu_table(otumat, taxa_are_rows=TRUE),
+                        tax_table(taxmat),
+                        #phy_tree(),
+                        refseq(refseqs) 
+                    )
         }
 
     return(ps)
