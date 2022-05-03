0b3b363190c1552804b830231bec11173d3e8696
J <j.williams20@imperial.ac.uk>
2022-05-03 13:55:49 +0100

    Update AssessIdtaxaPerformanceOnSimulatedSequences.r
    Add assessment of unclassifieds

--- SupportScripts/AssessIdtaxaPerformanceOnSimulatedSequences.r
+++ SupportScripts/AssessIdtaxaPerformanceOnSimulatedSequences.r
@@ -2,6 +2,9 @@
 
 set.seed(1)
 
+    library(DECIPHER)
+    library(tidyverse)
+
 #parameters
     path<-"../../BioinformaticPipeline_Env/Data/SimulatedData/IntermediateOutputs"
 
@@ -13,7 +16,7 @@ set.seed(1)
     IdtaxaThresholds<-seq(from=0, to=100, by =10)
 
     #either FALSE or the proportion
-    #MislabelledSequences<-
+    MislabelledSequences<-0.05
 
 #functions
     #duplicated from TrainMidori+Biocode classifier script
@@ -81,6 +84,7 @@ set.seed(1)
 
 
 
+    dna<-readDNAStringSet("../../BioinformaticPipeline_Env/Data/SimulatedData/AllSimulatedSequences.fasta")
 
 
 QueryIndices<-sample(size=floor(length(dna)*queryfraction), x=1:length(dna), replace=FALSE)
@@ -97,22 +101,37 @@ QueryDna<-dna[QueryIndices]
 
 
 fractions_df<-data.frame( Rank= c(),
-                    PercentCorrect= c(),
-                    PercentIncorrect= c(),
+                    PercentAllCorrectlyClassified= c(),
+                    PercentAllIncorrectlyClassified= c(),
+                    PercentAllCorrectlyUnclassified= c(),
+                    PercentAllIncorrectlyUnclassified= c(),
                     FractionBarcoded= c(),
                     Threshold= c()
                     )
 
 for (barcodedFraction in fractions) {
+
     RefIndices<-sample(size=floor(length(dna)*barcodedFraction), x=1:length(dna), replace=FALSE)
     RefDna<-dna[RefIndices]
+    if (MislabelledSequences!=FALSE) {
+        numMislabelled<-ceiling(length(RefDna)*MislabelledSequences)
+        MislabelledIndices<-sample(size=numMislabelled, x=1:length(RefDna), replace=FALSE)
+        NewLabelIndices<-sample(size=numMislabelled, x=1:length(dna), replace=FALSE)
 
+        #CorrectNamesOfMislabelled<-names(RefDna)[MislabelledIndices]
+        names(RefDna)[MislabelledIndices]<-names(dna)[NewLabelIndices]
+    }
 
     #BLAST
         BlastRefDna<-RefDna
         RefDnaIdMap<-data.frame(RefSeqTaxa=names(BlastRefDna),
                                 RefSeqID=paste0("RefEsv_", 1:length(BlastRefDna) ))
         names(BlastRefDna)<- RefDnaIdMap$RefSeqID
+
+        #get data frame of all taxa in the reference library
+        RefSeqTaxa_df<-plyr::ldply(str_split(RefDnaIdMap$RefSeqTaxa, ";"), rbind)
+        names(RefSeqTaxa_df)<- c("Root_True",paste0("Rank_", 1:numranks, "_True") )
+
         RefDnaFasta<-file.path(path, paste0( barcodedFraction, "RefSimulatedSequences.fasta"))
 
         writeXStringSet(BlastRefDna, RefDnaFasta, format="fasta", width=10000)
@@ -146,30 +165,32 @@ for (barcodedFraction in fractions) {
 
         #blast thresholding loop
         BLASTthresholds_df<-data.frame( Rank= c(),
-                                        PercentCorrect= c(),
-                                        PercentIncorrect= c(),
+                                        PercentAllCorrectlyClassified= c(),
+                                        PercentAllIncorrectlyClassified= c(),
+                                        PercentAllCorrectlyUnclassified= c(),
+                                        PercentAllIncorrectlyUnclassified= c(),
                                         FractionBarcoded= c(),
                                         Threshold= c(),
                                         Assignment=c()
                                         )
 
+            Taxa_df<-cbind(  plyr::ldply(str_split(blastTopHits$RefSeqTaxa, ";"), rbind), 
+                    plyr::ldply(str_split(blastTopHits$QuerySeqID, ";"), rbind)
+            )
+            names(Taxa_df)<-c("Root_Assigned", paste0("Rank_", 1:numranks, "_Assigned"), "Root_True",paste0("Rank_", 1:numranks, "_True") )
+            Blast_df<-cbind(Taxa_df, blastTopHits[,3:5])
 
         for (threshold in BLASTthresholds) {
 
             #only take those above blast threshold
-            blastTopHitsThresholded<-blastTopHits [blastTopHits$Blast_percentIdentical>threshold , ]
-
-
-            Taxa_df<-cbind(  plyr::ldply(str_split(blastTopHitsThresholded$RefSeqTaxa, ";"), rbind), 
-                    plyr::ldply(str_split(blastTopHitsThresholded$QuerySeqID, ";"), rbind)
-            )
-            names(Taxa_df)<-c("Root_Assigned", paste0("Rank_", 1:numranks, "_Assigned"), "Root_True",paste0("Rank_", 1:numranks, "_True") )
-            Blast_df<-cbind(Taxa_df, blastTopHitsThresholded[,3:5])
-
+            blastTopHitsThresholded<-Blast_df [Blast_df$Blast_percentIdentical>=threshold , ]
+            blastTopHitsUnclassified<-Blast_df [Blast_df$Blast_percentIdentical<threshold , ]
 
             BLASTranks_df<-data.frame(  Rank= paste0(1:numranks, "_Rank"),
-                                        PercentCorrect= rep(NA, numranks),
-                                        PercentIncorrect= rep(NA, numranks),
+                                        PercentAllCorrectlyClassified= rep(NA, numranks),
+                                        PercentAllIncorrectlyClassified= rep(NA, numranks),
+                                        PercentAllCorrectlyUnclassified= rep(NA, numranks),
+                                        PercentAllIncorrectlyUnclassified= rep(NA, numranks),
                                         FractionBarcoded= rep(barcodedFraction, numranks),
                                         Threshold= rep(threshold, numranks),
                                         Assignment=rep("BLAST", numranks)
@@ -177,8 +198,18 @@ for (barcodedFraction in fractions) {
 
 
             for (rank in 1:numranks) {
-                BLASTranks_df$PercentCorrect[rank] <- sum ( Blast_df[[paste0("Rank_", rank, "_Assigned")]]==Blast_df[[paste0("Rank_", rank, "_True")]] ) / dim(blastTopHits)[1]
-                BLASTranks_df$PercentIncorrect[rank] <-sum ( !Blast_df[[paste0("Rank_", rank, "_Assigned")]]==Blast_df[[paste0("Rank_", rank, "_True")]] ) / dim(blastTopHits)[1]
+                #percent of all
+                BLASTranks_df$PercentAllCorrectlyClassified[rank] <- sum ( blastTopHitsThresholded[[paste0("Rank_", rank, "_Assigned")]]==blastTopHitsThresholded[[paste0("Rank_", rank, "_True")]] ) / dim(Blast_df)[1]
+                BLASTranks_df$PercentAllIncorrectlyClassified[rank] <-sum ( !blastTopHitsThresholded[[paste0("Rank_", rank, "_Assigned")]]==blastTopHitsThresholded[[paste0("Rank_", rank, "_True")]] ) / dim(Blast_df)[1]
+                BLASTranks_df$PercentAllIncorrectlyUnclassified[rank] <- sum ( blastTopHitsUnclassified[[paste0("Rank_", rank, "_True")]]  %in%   RefSeqTaxa_df[[paste0("Rank_", rank, "_True")]] ) / dim(Blast_df)[1]
+                BLASTranks_df$PercentAllCorrectlyUnclassified[rank] <- sum ( !blastTopHitsUnclassified[[paste0("Rank_", rank, "_True")]]  %in%   RefSeqTaxa_df[[paste0("Rank_", rank, "_True")]] ) / dim(Blast_df)[1]
+
+                #percent of classfied/unclassified
+                BLASTranks_df$PercentClassifiedCorrectlyClassified[rank] <- sum ( blastTopHitsThresholded[[paste0("Rank_", rank, "_Assigned")]]==blastTopHitsThresholded[[paste0("Rank_", rank, "_True")]] ) / length(blastTopHitsThresholded[[paste0("Rank_", rank, "_Assigned")]])
+                BLASTranks_df$PercentClassifiedIncorrectlyClassified[rank] <-sum ( !blastTopHitsThresholded[[paste0("Rank_", rank, "_Assigned")]]==blastTopHitsThresholded[[paste0("Rank_", rank, "_True")]] ) / length(blastTopHitsThresholded[[paste0("Rank_", rank, "_Assigned")]])
+                BLASTranks_df$PercentUnclassifiedIncorrectlyUnclassified[rank] <- sum ( blastTopHitsUnclassified[[paste0("Rank_", rank, "_True")]]  %in%   RefSeqTaxa_df[[paste0("Rank_", rank, "_True")]] ) / length(blastTopHitsUnclassified[[paste0("Rank_", rank, "_Assigned")]])
+                BLASTranks_df$PercentUnclassifiedCorrectlyUnclassified[rank] <- sum ( !blastTopHitsUnclassified[[paste0("Rank_", rank, "_True")]]  %in%   RefSeqTaxa_df[[paste0("Rank_", rank, "_True")]] ) / length(blastTopHitsUnclassified[[paste0("Rank_", rank, "_Assigned")]])
+
             }
             
             BLASTthresholds_df<-rbind(BLASTranks_df, BLASTthresholds_df)
@@ -190,8 +221,10 @@ for (barcodedFraction in fractions) {
 
 
         thresholds_df<-data.frame( Rank= c(),
-                            PercentCorrect= c(),
-                            PercentIncorrect= c(),
+                            PercentAllCorrectlyClassified= c(),
+                            PercentAllIncorrectlyClassified= c(),
+                            PercentAllCorrectlyUnclassified= c(),
+                            PercentAllIncorrectlyUnclassified= c(),
                             FractionBarcoded= c(),
                             Threshold= c(),
                             Assignment=c()
@@ -233,19 +266,35 @@ for (barcodedFraction in fractions) {
             IdtaxaDf<-merge(merge(TaxonDf, ConfDf, by="ESV"), TrueIdDf, by="ESV")
 
                 ranks_df<-data.frame( Rank= paste0(1:numranks, "_Rank"),
-                            PercentCorrect= rep(NA, numranks),
-                            PercentIncorrect= rep(NA, numranks),
+                            PercentAllCorrectlyClassified= rep(NA, numranks),
+                            PercentAllIncorrectlyClassified= rep(NA, numranks),
+                            PercentAllCorrectlyUnclassified= rep(NA, numranks),
+                            PercentAllIncorrectlyUnclassified= rep(NA, numranks),
                             FractionBarcoded= rep(barcodedFraction, numranks),
                             Threshold= rep(threshold, numranks),
                             Assignment=rep("Idtaxa", numranks)
                             )
 
                 for (ranknum in 1:numranks) {
-
-                    ranks_df$PercentCorrect[ranknum] <- sum( IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]] == IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]], na.rm=TRUE) / dim(IdtaxaDf)[1]
-
+                    
+                    #percent all
                     assignedSeqsIndices<-   !(is.na(IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]]) | lapply(str_split(IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]], "_"), '[', 1)  == "unclassified")
-                    ranks_df$PercentIncorrect[ranknum] <- sum( IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]][assignedSeqsIndices] != IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]][assignedSeqsIndices], na.rm=TRUE) / dim(IdtaxaDf)[1]
+                    ranks_df$PercentAllCorrectlyClassified[ranknum] <- sum( IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]] == IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]], na.rm=TRUE) / dim(IdtaxaDf)[1]
+                    ranks_df$PercentAllIncorrectlyClassified[ranknum] <- sum( IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]][assignedSeqsIndices] != IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]][assignedSeqsIndices], na.rm=TRUE) / dim(IdtaxaDf)[1]
+                                        
+                    unassignedSeqsIndices<-   (is.na(IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]]) | lapply(str_split(IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]], "_"), '[', 1)  == "unclassified")
+                    ranks_df$PercentAllIncorrectlyUnclassified[ranknum] <- sum( IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]][unassignedSeqsIndices] %in% RefSeqTaxa_df[[paste0("Rank_", ranknum, "_True")]] ) / dim(IdtaxaDf)[1]
+                    ranks_df$PercentAllCorrectlyUnclassified[ranknum] <- sum( !IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]][unassignedSeqsIndices] %in% RefSeqTaxa_df[[paste0("Rank_", ranknum, "_True")]] ) / dim(IdtaxaDf)[1]
+
+                    #percent classified/unclassified
+                    assignedSeqsIndices<-   !(is.na(IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]]) | lapply(str_split(IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]], "_"), '[', 1)  == "unclassified")
+                    ranks_df$PercentClassifiedCorrectlyClassified[ranknum] <- sum( IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]] == IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]], na.rm=TRUE) / sum(assignedSeqsIndices)
+                    ranks_df$PercentClassifiedIncorrectlyClassified[ranknum] <- sum( IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]][assignedSeqsIndices] != IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]][assignedSeqsIndices], na.rm=TRUE) / sum(assignedSeqsIndices)
+                                        
+                    unassignedSeqsIndices<-   (is.na(IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]]) | lapply(str_split(IdtaxaDf[[ paste0("Rank_", ranknum, "_Assigned") ]], "_"), '[', 1)  == "unclassified")
+                    ranks_df$PercentUnclassifiedIncorrectlyUnclassified[ranknum] <- sum( IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]][unassignedSeqsIndices] %in% RefSeqTaxa_df[[paste0("Rank_", ranknum, "_True")]] ) / sum(unassignedSeqsIndices)
+                    ranks_df$PercentUnclassifiedCorrectlyUnclassified[ranknum] <- sum( !IdtaxaDf[[ paste0("Rank_", ranknum, "_True") ]][unassignedSeqsIndices] %in% RefSeqTaxa_df[[paste0("Rank_", ranknum, "_True")]] ) / sum(unassignedSeqsIndices)
+
             }
             
             thresholds_df<-rbind(ranks_df, thresholds_df)
@@ -265,7 +314,7 @@ for (barcodedFraction in fractions) {
 
 
 
-pdf(file = "/home/j/Desktop/AssignedSimulatedESVs.pdf", width=20, height =8 ) # The height of the plot in inches    
+pdf(file = paste0("/home/j/Desktop/AssignedSimulatedESVs_MislabelledSequences_", MislabelledSequences, ".pdf"), width=20, height =8 ) # The height of the plot in inches    
 
 fractions_df$assignmentThreshold<-paste0(fractions_df$Assignment , fractions_df$Threshold)
 fractions_df$RankAssignment<-paste0(fractions_df$Assignment , fractions_df$Rank)
@@ -276,14 +325,37 @@ fractions_df$ThreholdTightness<- as.numeric(as.character(fractions_df$ThreholdTi
 
 
 
-ggplot(fractions_df, aes(x=PercentIncorrect, y=PercentCorrect, color=Rank, shape=Assignment)) +
+ggplot(fractions_df, aes(x=PercentAllIncorrectlyClassified, y=PercentAllCorrectlyClassified, color=Rank, shape=Assignment)) +
     geom_path(aes(group=RankAssignment, linetype=Assignment), size=1, alpha=0.7) +
     geom_point(aes(color=Rank, size=ThreholdTightness)) +
-    #geom_text(aes(label=as.character(Threshold)),hjust=0,vjust=0, size=2, color="Black") +
+    geom_text(aes(label=as.character(Threshold)),hjust=0,vjust=0, size=2, color="Black") +
     coord_cartesian(xlim = c(0, 1), ylim=c(0, 1)) +
     facet_wrap(~FractionBarcoded) +
     ggtitle("Simulated ESVs assigned using Idtaxa and BLAST for different fractions of lowest rank groups barcoded - larger icons indicate looser thresholds for Idtaxa (0-100 confidence) and BLAST (85-97% similarity)")
 
+ggplot(fractions_df, aes(x=PercentAllIncorrectlyUnclassified, y=PercentAllCorrectlyUnclassified, color=Rank, shape=Assignment)) +
+    geom_path(aes(group=RankAssignment, linetype=Assignment), size=1, alpha=0.7) +
+    geom_point(aes(color=Rank, size=ThreholdTightness)) +
+    geom_text(aes(label=as.character(Threshold)),hjust=0,vjust=0, size=2, color="Black") +
+    coord_cartesian(xlim = c(0, 1), ylim=c(0, 1)) +
+    facet_wrap(~FractionBarcoded) +
+    ggtitle("Simulated ESVs unassigned using Idtaxa and BLAST for different fractions of lowest rank groups barcoded - larger icons indicate looser thresholds for Idtaxa (0-100 confidence) and BLAST (85-97% similarity)")
+
+ggplot(fractions_df, aes(x=ThreholdTightness, y=PercentClassifiedCorrectlyClassified, color=Rank, shape=Assignment)) +
+    geom_path(aes(group=RankAssignment, linetype=Assignment), size=1, alpha=0.7) +
+    geom_point(aes(color=Rank, size=ThreholdTightness)) +
+    geom_text(aes(label=as.character(Threshold)),hjust=0,vjust=0, size=2, color="Black") +
+    #coord_cartesian(xlim = c(0, 1), ylim=c(0, 1)) +
+    facet_wrap(~FractionBarcoded) +
+    ggtitle("Assigned Simulated ESVs correct and incorrect using Idtaxa and BLAST for different fractions of lowest rank groups barcoded - larger icons indicate looser thresholds for Idtaxa (0-100 confidence) and BLAST (85-97% similarity)")
+
+ggplot(fractions_df, aes(x=ThreholdTightness, y=PercentUnclassifiedCorrectlyUnclassified, color=Rank, shape=Assignment)) +
+    geom_path(aes(group=RankAssignment, linetype=Assignment), size=1, alpha=0.7) +
+    geom_point(aes(color=Rank, size=ThreholdTightness)) +
+    geom_text(aes(label=as.character(Threshold)),hjust=0,vjust=0, size=2, color="Black") +
+    #coord_cartesian(xlim = c(0, 1), ylim=c(0, 1)) +
+    facet_wrap(~FractionBarcoded) +
+    ggtitle("Unassigned ESVs correct and incorrect using Idtaxa and BLAST for different fractions of lowest rank groups barcoded - larger icons indicate looser thresholds for Idtaxa (0-100 confidence) and BLAST (85-97% similarity)")
 
 
 dev.off()
