b860ad66907f96f99331db68958d26cae40317fe
J <j.williams20@imperial.ac.uk>
2023-01-24 17:32:18 +0000

    IdTaxa Caching implemented
    Run_IDTAXA() adjusted to berak query sequences into chunks and cache out after each chunk complete (as per treatment of runs in run_DADA2()).

--- Pipeline/Functions/1_RunIDTAXA().R
+++ Pipeline/Functions/1_RunIDTAXA().R
@@ -2,7 +2,7 @@
 
 #expects classifier trained with seq names in following format, number of ranks can vary: "Root; Arthropoda; Arachnida; Araneae; Uloboridae; Zosis; geniculata" 
 
-RunIdtaxa<-function(Type, trainingSet, TableToMergeTo, SeqsToAssign=SeqsToAssign, threshold) {
+RunIdtaxa<-function(Type, trainingSet, TableToMergeTo, SeqsToAssign=SeqsToAssign, threshold, QuerySequenceChunkSize=10000) {
     if (Type == "Assign") {
         #SeqsToAssign should modify which seqs are selected here
             if (SeqsToAssign=="ESVs"){
@@ -21,15 +21,34 @@ RunIdtaxa<-function(Type, trainingSet, TableToMergeTo, SeqsToAssign=SeqsToAssign
         #load the specified trainingset
         load(file.path(path, "Data", "Classifiers", trainingSet))
 
-        #classify
-            ids <- IdTaxa(dna,
-                        trainingSet,
-                        type="extended",
-                        strand="both",
-                        threshold=threshold,
-                        processors=NULL)
+        #first check if any chunks completed in a previous exited run, if so load these and append  additional chunks to this object
+            if(file.exists(file=file.path(path, "IntermediateOutputs", paste0(dataname,"_ids.RDS")))) {
+                ids<-readRDS(file=file.path(path, "IntermediateOutputs", paste0(dataname,"_ids.RDS") ))
+            } else {
+                ids<-list()
+            }
 
-            #if rank in ids
+        # splitquery sequences into chunks
+            dna_list<-split(dna, ceiling(seq_along(dna)/QuerySequenceChunkSize))
+            print(paste0("Query sequences split into ", length(dna_list), " chunks of ", QuerySequenceChunkSize, " sequences"))
+            print(paste0("Starting classification at chunk ", length(ids)+1))
+            for (chunkNumber in (length(ids)+1):length(dna_list)) {
+                #classify
+                ids[[chunkNumber]] <- IdTaxa(dna_list[[chunkNumber]],
+                                        trainingSet,
+                                        type="extended",
+                                        strand="both",
+                                        threshold=threshold,
+                                        processors=NULL)
+                print(paste0("Chunk ", chunkNumber, " complete"))
+                CacheOutput(ids)
+            }
+            print(paste0("All chunks complete"))
+            ids<-do.call(c, ids)
+            print(paste0("All chunks merged"))
+
+
+        #if rank in ids
             if ('rank' %in% colnames(as.data.frame(ids[1][[1]]))) {
 
                 ReformatIds<-   function (id) {
@@ -45,7 +64,7 @@ RunIdtaxa<-function(Type, trainingSet, TableToMergeTo, SeqsToAssign=SeqsToAssign
                 IdtaxaDf<-plyr::ldply(output_list, rbind)
                 names(IdtaxaDf)[1]<-"ESV"
 
-            #if rank not in ids
+        #if rank not in ids
             } else {
 
                 ExtractFromIds<-function(list_item, category){
