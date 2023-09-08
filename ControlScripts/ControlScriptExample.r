3603f4c38772f22df2cf0ffc30b7e4145a930d19
J <j.williams20@imperial.ac.uk>
2023-09-08 17:04:15 +0100

    Create single example control script

--- ControlScripts/ControlScriptExample.r
+++ ControlScripts/ControlScriptExample.r
@@ -1,53 +1,48 @@
-#settings
-
-    #cluster settings
-        #on HPC?
-        HPC<-TRUE
-        if (HPC==TRUE)   {setwd("/rds/general/user/jcw120/home/BioinformaticPipeline_Env")} #necessary as it appears different job classes have different WDs.
-
-        #CRAN mirror
-            r = getOption("repos")
-            r["CRAN"] = "http://cran.us.r-project.org"
-            options(repos = r)
-
+#Control Script Template
     # general settings
-        path <-"../BioinformaticPipeline_Env" # HPC
-        #path <-"../../BioinformaticPipeline_Env" #for personal machine
+        HPC<-FALSE
+            # set whether you are working on an HPC or your local machine
 
+        path <-"<YourPath>/BioinformaticPipeline_Env"
             # this should be the path to the working directory within which you have the following folders:
                 # BioinformaticPipeline - get this from github and then create your own control file from this template - don't modify anything else
-                # FASTQs - fill this file with your unmerged multiplexed raw FASTQ files
-                # ReferenceLibraries - fill this file with your taxonomic reference library formatted either as i) ... or ii) ...
-                # IntermediateOutputs - this will be populated by the pipeline as it runs, it will enable the pipeline to be run over multiple sessions as the output from each module is saved here.
+                # FASTQs -directory with your unmerged multiplexed raw FASTQ files, in a subdirectory named to match dataname argument
+                # ReferenceLibraries - directory with your taxonomic reference library formatted as i) IDTAXA classsifier and/or ii) BLAST database
+                # IntermediateOutputs - this directory will be populated by the pipeline as it runs, it will enable the pipeline to be run over multiple sessions as the output from each module is saved here.
                 # Results - this is where final results will be saved
-        dataname="Rosie2021_18s"
-            # this should be the name you associate with this set of fastqs,  the input fastq folder should be labelled with this
+        dataname="Example"
+            # this should be the name you associate with this set of fastqs,  the inout fastq folder should be labelled with this
             # and all output and result files will be labelled with this name.
-        multithread=TRUE
-            #on windows set multithread=FALSE
+        multithread=4
+            # number of threads to use on parallelised steps within the pipeline
 
     # Cutadapt settings
         UseCutadapt=FALSE
             # either TRUE or FALSE: determines whether cutadapt is run
-        FWD ="ACCTGCGGARGGATCA"
-            ## CHANGE ME to your forward primer sequence
-        REV = "GAGATCCRTTGYTRAAAGTT"
-            ## CHANGE ME...
+        FWD ="ACTG"
+            ## your forward primer sequence
+        REV = "ACTG"
+            ## your reverse primer sequence
 
     # DADA2 settings
-        truncLen=c(140, 140)
-            #the length at which the sequences should be truncated (forward and reverse)
-        trimLeft=c(15,20)
-            # the # of bases to remove from the start of the sequences
+        NumberOfRuns<-2
+            #Number of sequencing runs used to generate the data for this experiment, e.g. 1 or 2
+        truncLen=list(c(150,150), c(150,150))
+            #the lengths at which the sequences should be truncated (forward and reverse) for each run. If single-end or pre-merged 
+            # sequences are input only a single number is required for each entry in the list.
+        trimLeft=list(c(0,0), c(0,0))
+            # the # of bases to remove from the start of the sequences (forward and reverse) for each run. If single-end or pre-merged 
+            # sequences are input only a single number is required for each entry in the list.
         maxN=0
-            # after truncation seqs with more than this number of Ns are discarded (DADA2 does not allow Ns)
+            # after truncation seqs with more than this number of Ns are discarded (DADA2 does not allow Ns so this must be kept to 0 if DADA2 outputs are desired)
         maxEE=c(2,2)
             #  After truncation, reads with higher than ‘maxEE’ "expected errors"  
-            # will be discarded. Expected errors are calculated from the nominal definition 
-            # of the quality score: EE = sum(10^(-Q/10))
+            # will be discarded (forward and reverse). Expected errors are calculated from the nominal definition 
+            # of the quality score: EE = sum(10^(-Q/10)). If single-end or pre-merged 
+            # sequences are input only a single number is required.
         truncQ=2
             # Truncate reads at the first instance of a quality score less than or equal to ‘truncQ’. 
-        DesiredSequenceLengthRange=87:186
+        DesiredSequenceLengthRange=NULL
             #sequence length range to keep enter as e.g. 360:270, if NULL all sequence lengths are kept. 
             # Sequences that are much longer or shorter than expected
             # may be the result of non-specific priming. This removal is analogous to “cutting a band” 
@@ -55,6 +50,14 @@
         pool="pseudo"
             #TRUE, FALSE, or pseudo. pseudo pooling approximates the effect of denoising with pooled samples, but with
             # linearly increasing computational time (ca. doubled compared to no pooling)
+        MixedOrientation="FALSE"
+            # TRUE or FALSE. FALSE (default) means dada2 proceeds in the normal fashion. TRUE means that _RO and _FO 
+            # suffixes will be looked for in the sample names to enable samples containing reverse orientation (RO) sequences 
+            # to be reverse complemented and then merged into the matching sample containing the forward orientated sequences.
+        ReadType="Paired-end"
+            # default "Paired-end". Other options: "Single-read" and "Premerged". Premerged refers to paired-end data which has been merged prior to input into the pipeline.
+            # This is not recommended but in some historic datasets it is unavoidable. In this case DADA2's inflateErr(inflation=3) function is used to correct distortions
+            # to error rates introduced by premerging the paired-end reads.
 
     # lulu settings1
         MatchRate1=90 #as a %, default 84
@@ -83,7 +86,7 @@
             #number of base differences at which swarm clustering will be performed (1=default)                               
         SimilarityThreshold = 0.97
             #%age similarity at which to cluster sequences as a decimal
-        threads=32
+        threads=20
             # number of threads available 
 
     # lulu settings2
@@ -107,42 +110,46 @@
         #  well-separated, but co-occuring, sequence similar species. 
 
     # IDTAXA settings
-        Type ="Assign"  
-            #whether to "Create" or "Load" a training set, or perform "No Assignment"
-        desiredranks<-c("rootrank", "domain", "superkingdom", "kingdom", "subkingdom", "superphylum", "phylum", "subphylum", "infraphylum", "superclass", "class", "subclass", "infraclass", "superorder",  "order", "suborder", "superfamily", "family", "subfamily", "genus")
-            #determine this based on the training set you are using (accessible with trainingSet[[3]]). If this trainingSet
+        IDTAXA =TRUE
+            #whether to run IDTAXA assignment, TRUE or FALSE
+        desiredranks<-c("rootrank", "domain", "phylum", "class", "order", "family", "genus")
+            #determine this based on the training set you are using (accessible with trainingSet[[3]] once the trainingSet is loaded into R). If this trainingSet
             #does not contain ranks then this is ignored, and it is assumed all reference sequences have an assignment at 
             #all ranks. If this is not the case assignment will be unusable. If the trainingSet does contain ranks then only 
-            #the ranks specified here are reported, this enables comparison between assignments is possible. e.g. looking at class 
-            #assignments for all sequences.
-            #some examples for 18s ref libraries:
+            #the ranks specified here are reported, this enables easy comparison between assignments as all sequences receive the same number of taxxonomic ranks
+            # . e.g. looking at class assignments for all sequences.
+            #some examples for widely used reference libraries:
                 #SILVA 18s
                     #desiredranks<-c("rootrank", "domain", "superkingdom", "kingdom", "subkingdom", "superphylum", "phylum", "subphylum", "infraphylum", "superclass", "class", "subclass", "infraclass", "superorder",  "order", "suborder", "superfamily", "family", "subfamily", "genus")
                 #PR2 18s
                     #desiredranks<-c("rootrank", "kingdom", "division", "phylum", "class", "order", "family", "genus")
                 #GTDB 16s
                     #desiredranks<-c("rootrank", "domain", "phylum", "class", "order", "family", "genus")
-        trainingSet= "SILVA_SSU_r138_2019.RData" 
-        #trainingSet="SILVA_SSU_r138_2019.RData"
-            #ref library to load if loading
+        trainingSet= "GTDB_r202-mod_April2021.RData"
+            #IDTAXA classifier to use, should be the file name for your pretrained classifier. Note these commonly used pre-trained classifiers can be downloaded here:
+            # http://www2.decipher.codes/Downloads.html
         SeqsToAssign ="ESVs"
-            #whether to assign to "ESVs", "OTUs", or "cOTUs"
-        threshold=30 
+            #whether to make assignments to "ESVs", "OTUs", or "cOTUs". default of assigning to ESVs is recommended to make full use of multi-algorithm agreement.
+        threshold=40 
             # %age confidence of assignment required to record assignment
-            #30=low confidence, 40=moderate, 50 = high, 60= very high
-
+            # 30=low confidence, 40=moderate, 50 = high, 60= very high
+        parallel=FALSE
+            #whether idtaxa should be run in parallel using the number of cores specififed with the multithread argument.
+            #each core will classify 500 sequences at a time using parallel::mclapply. Setting this to TRUE is recommended 
+            # on an an HC with more than 10 cores. Note that this is a workaround as Idtaxa's inbuilt parallelisation seems
+            # unable to use more than around 10 cores.
     #BLAST
-        Blast= "No" 
-            #run blast or not, "Yes" to run it!
+        Blast= FALSE
+            #run blast or not, TRUE or FALSE
         dbname= "MidoriBiocodeBlastDB" 
-            # name of the blast db (unsure what it is fpr silva)
-        assignmentThresholds=c(0, 0, 0, 0, 0, 80, 80, 80, 80, 85, 85, 85, 85, 90, 90, 90, 95, 95, 95, 97)
+            # name of the blast db you wish to use, should be the directory name of your prebuilt blast database
+        assignmentThresholds=c(0, 0, 80, 85, 90, 95, 97)
             #similarity thresholds at which blast assignments should be made. 
-            # read from top rank to bottom rank. Requires a value for each rank in dataset.
+            # read from highest rank to lowest rank. Requires a value for each rank in dataset.
         
 
 # run pipeline
-    source(file.path(path, "BioinformaticPipeline", "Pipeline", "CDLCLA_Pipeline.R"))
+    source(file.path(path, "BioinformaticPipeline", "Pipeline", "Pipeline.R"))
 
 
 
